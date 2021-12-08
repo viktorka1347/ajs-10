1fcf00fc17dff71090048cb3ba52bfbceff9572e
Viktoria Ulyanchenko <viktorka1347@gmail.com>
2021-12-08 20:11:40 +0300

    main
    Signed-off-by: Viktoria Ulyanchenko <viktorka1347@gmail.com>

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -2,7 +2,7 @@ import json from "./parser";
 import read from "./reader";
 
 export default class GameSavingLoader {
-    static load() {
-        return read().then((response) => json(response));
+    static async load() {
+        return JSON.parse(await json(await read()));
     }
 }
